b64456c561daecf491ab01a5f4ae755abad98e05
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-05-06 12:36:00 +0300

    feat(video): add video resizing via FFmpeg and API endpoint

--- src/controllers/video.js
+++ src/controllers/video.js
@@ -104,6 +104,35 @@ const extractAudio = async (req, res, handleErr) => {
   }
 };
 
+// Resize a video file (creates a new video file)
+const resizeVideo = async (req, res, handleErr) => {
+  const videoId = req.body.videoId;
+  const width = Number(req.body.width);
+  const height = Number(req.body.height);
+
+  DB.update();
+  const video = DB.videos.find((video) => video.videoId === videoId);
+  video.resizes[`${width}x${height}`] = { processing: true };
+
+  const originalVideoPath = `./storage/${video.videoId}/original.${video.extension}`;
+  const targetVideoPath = `./storage/${video.videoId}/${width}x${height}.${video.extension}`;
+
+  try {
+    await FF.resize(originalVideoPath, targetVideoPath, width, height);
+
+    video.resizes[`${width}x${height}`].processing = false;
+    DB.save();
+
+    res.status(200).json({
+      status: "success",
+      message: "The video is now being processed!",
+    });
+  } catch (e) {
+    util.deleteFile(targetVideoPath);
+    return handleErr(e);
+  }
+};
+
 // Return a video asset to the client
 const getVideoAsset = async (req, res, handleErr) => {
   const videoId = req.params.get("videoId");
@@ -181,6 +210,7 @@ const controller = {
   getVideos,
   uploadVideo,
   extractAudio,
+  resizeVideo,
   getVideoAsset,
 };
 
--- src/router.js
+++ src/router.js
@@ -32,6 +32,9 @@ module.exports = (server) => {
   // Extract the audio for a video file (can only be done once per video)
   server.route("patch", "/api/video/extract-audio", Video.extractAudio);
 
+  // Resize a video file (creates a new video file)
+  server.route("put", "/api/video/resize", Video.resizeVideo);
+
   // Return a video asset to the client
   server.route("get", "/get-video-asset", Video.getVideoAsset);
 };
